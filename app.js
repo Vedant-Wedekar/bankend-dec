78f473e73c315c27f9fc6893e51bb906aafdfd49
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-01-01 20:18:11 +0530

    main file for rendering

--- app.js
+++ app.js
@@ -11,13 +11,10 @@
 //     res.send('this is a product');
 //   })
 
-  
-
 // app.post('/get-form-data',(req, res) => {
 // console.log(req.body);
 // res.send(' data received');
 // })
-  
 
 // // Start the server
 // app.listen(3000, () => {
@@ -27,17 +24,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const userModel = require('./models/user');// Connect to MongoDB
+const connection = require('./config/db');
+
+
+app.use(express.static('public'));
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
 // Middleware for logging
-app.use((req, res, next) => {
-    const a = 10;
-    const b = 10;
-    console.log('Sum of a and b:', a + b);
-    next();
-});
+// app.use((req, res, next) => {
+//     const a = 10;
+//     const b = 10;
+//     console.log('Sum of a and b:', a + b);
+//     next();
+// });
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -52,6 +54,27 @@ app.post('/get-form-data', (req, res) => {
     console.log(req.body); // req.body contains form data
     res.send('Data received');
 });
+
+
+
+app.get('/register', (req, res) => {
+    res.render('register');
+  });
+
+
+// Route to handle registration form submission 
+app.post('/register', async (req, res) => {
+
+    const { username, email, password } = req.body;
+    const newUser = await userModel.create({
+        username : username,
+        phone : phone,
+        email : email,
+        password : password,
+    });
+    res.send(newUser);
+  });
+
 
 // Another route example
 app.get('/product', (req, res) => {
